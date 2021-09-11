41c6a13cddb1f5036bd47af3ae0abe0a8c570786
Mauricio Oliveira <ciumauri@gmail.com>
2021-09-11 15:18:46 -0300

    Inclusão de novos arquivos de JS

--- rocketseat/discover/js/functions.js
+++ rocketseat/discover/js/functions.js
@@ -1,18 +1,32 @@
-/*
-    Function() constructor
+//Manipulando Arrays
 
-    * expressão new
-    * criar um novo objeto
-    * this keyword
-*/
+let techs = ["html", "css", "js","dart", "c", "c++"]
+console.log(`Os elementos iniciais são: ${techs}`)
 
-function Person(name, idade) {
-  this.name = name
-  this.idade = idade
-  this.walk = ()=> this.name + " está andando, e tem " + this.idade + " anos."
-}
+//adicionar um item no fim
+techs.push("nodejs")
+console.log(`Após adição de um novo item no fim: ${techs}`)
 
-const mauricio = new Person('Mauricio', 38)
-const anne = new Person('Anne', 33)
-console.log(mauricio.walk())
-console.log(anne.walk())
+//adicionar no começo
+techs.unshift("sql")
+console.log(`Após adição de um novo item no começo: ${techs}`)
+
+//remover do fim(ultimo elemento)
+techs.pop()
+console.log(`Após remoção do último item: ${techs}`)
+
+//remover do inicio(primeiro elemento)
+techs.shift()
+console.log(`Após remoção do primeiro item: ${techs}`)
+
+//pegar comente alguns elementos do array(posição, item(s))
+console.log(`Eu quero pegar os 2 últimos elementos que são: ${techs.slice(1, 3)}`)
+
+//remover 1 ou mais itens de qualquer posição do array(posição, item(s))
+techs.splice(1, 2)
+console.log(`Removi 2 item e ficou apenas o item(s): ${techs}`)
+
+//encontrar a posição de um elemento no array
+let index = techs.indexOf('c')
+console.log(`A posição da linguagem procurada é: ${index}`)
+
---
+++ rocketseat/discover/js/functions_9.js
@@ -0,0 +1,33 @@
+//Manipulação de Strings e Arrays
+
+//Separando Strings 
+let phrase = "Eu quero viver o Amor com Anne!"
+let myArray = phrase.split(" ")
+console.log(myArray)
+
+//Unindo Strings  
+let phraseUnderScore = myArray.join("_")
+console.log(phraseUnderScore.toUpperCase())
+
+//Verificação de textos
+console.log(phrase.includes("Anne"))
+
+//Contando caracteres e digitos
+let word = "paralelepipedo"
+console.log(`A palavra tem ${word.length} caracteres.`)
+let number = 1234
+console.log(`A quantidade de digitos é: ${String(number).length}`)
+
+//Criando Array como construtor
+//let arrayConst = ['a', 'b', 'c']
+let arrayConst = new Array('a', 'b', 'c')
+console.log(arrayConst)
+
+//Contar elementos de um array
+console.log(`O Array contém ${['a', 'b', 'c'].length} elementos`)
+
+//Manipulando Arrays
+//Transformar cadeia de caracteres em elementos de um Array
+
+let words = "manipulação"
+console.log(Array.from(words))
